676983bf1705622d90023e73f875559d7bc00aa6
CicadaN <88105680+CicadaN@users.noreply.github.com>
2024-05-01 21:30:44 +0300

    3 step. add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,17 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Override
+    public void run() {
         System.out.println("Hello World!");
     }
-
+    public static void main(String[] args) {
+        new CommandLine(new App()).execute(args);
+    }
 }
