8f42b69366a5fdecdb5d74100828c6e11c831ba4
CicadaN <88105680+CicadaN@users.noreply.github.com>
2024-05-30 22:01:34 +0300

    test git actions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/CicadaN/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/CicadaN/java-project-71/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/31368a68a163e658869f/maintainability)](https://codeclimate.com/github/CicadaN/java-project-71/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/31368a68a163e658869f/test_coverage)](https://codeclimate.com/github/CicadaN/java-project-71/test_coverage)
+## Result
+![result.jpg](app%2Fsrc%2Fmain%2Fresources%2Fresult.jpg)
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -12,33 +12,33 @@ public class Differ {
 
     public static Map<String, Object> parseJson(String filepath) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        byte[] FileContent = Files.readAllBytes(Paths.get(filepath));
-        return mapper.readValue(FileContent, Map.class);
+        byte[] fileContent = Files.readAllBytes(Paths.get(filepath));
+        return mapper.readValue(fileContent, Map.class);
     }
     public static String getData(String filepath1, String filepath2) throws Exception {
         Map<String, Object> map1 = parseJson(filepath1);
         Map<String, Object> map2 = parseJson(filepath2);
 
-        Map<String, Object> SortedMap1 = new TreeMap<>(map1);
-        Map<String, Object> SortedMap2 = new TreeMap<>(map2);
+        Map<String, Object> sortedMap1 = new TreeMap<>(map1);
+        Map<String, Object> sortedMap2 = new TreeMap<>(map2);
 
 
         StringBuilder result = new StringBuilder("{\n");
 
-        for (String key : SortedMap1.keySet()) {
-            if (!SortedMap2.containsKey(key)) {
-                result.append("  - ").append(key).append(": ").append(SortedMap1.get(key)).append("\n");
-            } else if (!SortedMap1.get(key).equals(SortedMap2.get(key))) {
-                result.append("  - ").append(key).append(": ").append(SortedMap1.get(key)).append("\n");
-                result.append("  + ").append(key).append(": ").append(SortedMap2.get(key)).append("\n");
+        for (String key : sortedMap1.keySet()) {
+            if (!sortedMap2.containsKey(key)) {
+                result.append("  - ").append(key).append(": ").append(sortedMap1.get(key)).append("\n");
+            } else if (!sortedMap1.get(key).equals(sortedMap2.get(key))) {
+                result.append("  - ").append(key).append(": ").append(sortedMap1.get(key)).append("\n");
+                result.append("  + ").append(key).append(": ").append(sortedMap2.get(key)).append("\n");
             } else {
-                result.append("    ").append(key).append(": ").append(SortedMap1.get(key)).append("\n");
+                result.append("    ").append(key).append(": ").append(sortedMap1.get(key)).append("\n");
             }
         }
 
-        for (String key : SortedMap2.keySet()) {
-            if (!SortedMap1.containsKey(key)) {
-                result.append("  + ").append(key).append(": ").append(SortedMap2.get(key)).append("\n");
+        for (String key : sortedMap2.keySet()) {
+            if (!sortedMap1.containsKey(key)) {
+                result.append("  + ").append(key).append(": ").append(sortedMap2.get(key)).append("\n");
             }
         }
 
