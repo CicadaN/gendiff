490836ae43631f868294b1e6426deca9cdb58b37
CicadaN <88105680+CicadaN@users.noreply.github.com>
2024-06-08 21:42:10 +0300

    test git actions

---
+++ app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    application
+    id("java")
+    id("checkstyle")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application { mainClass.set("hexlet.code.App") }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation ("info.picocli:picocli:4.7.6")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
